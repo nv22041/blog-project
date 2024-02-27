720239e4c7cf8a43ed9c21e2760485f5e6090359
nv22041 <160115267+nv22041@users.noreply.github.com>
2024-02-27 07:29:32 +0000

    2-setup project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css'
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className='text-red-500 text-5xl'>hello world</h1>
+      
+    </div>
+  );
+}
+
+export default App;
